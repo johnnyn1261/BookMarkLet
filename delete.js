043081fd4fc05e0dd07ad72df437fc14bdb2c834
johnnyn1261 <johnnyn1261@gmail.com>
2019-01-20 16:21:34 -0500

    New bookmarklet functionality

---
+++ delete.js
@@ -0,0 +1,44 @@
+(function() {
+  let paragraphs = document.getElementsByTagName('p');
+  for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].innerHTML = '';
+  }
+
+  let heading1 = document.getElementsByTagName('h1');
+  for (let i = 0; i < heading1.length; i++) {
+    heading1[i].innerHTML = '';
+  }
+
+  let heading2 = document.getElementsByTagName('h2');
+  for (let i = 0; i < heading2.length; i++) {
+    heading2[i].innerHTML = '';
+  }
+
+  let heading3 = document.getElementsByTagName('h3');
+  for (let i = 0; i < heading3.length; i++) {
+    heading3[i].innerHTML = '';
+  }
+
+  let heading4 = document.getElementsByTagName('h4');
+  for (let i = 0; i < heading4.length; i++) {
+    heading4[i].innerHTML = '';
+  }
+
+  let heading5 = document.getElementsByTagName('h5');
+  for (let i = 0; i < heading5.length; i++) {
+    heading5[i].innerHTML = '';
+  }
+
+  let heading6 = document.getElementsByTagName('h6');
+  for (let i = 0; i < heading6.length; i++) {
+    heading6[i].innerHTML = '';
+  }
+})();
+
+/*
+(function() {
+  let script = document.createElement('script');
+  script.src = 'delete.js';
+  document.body.appendChild(script);
+})();
+*/
